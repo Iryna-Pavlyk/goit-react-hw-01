4cfdb51429776f03a5688e9018b9ecd4abe08745
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-03-29 12:10:36 +0200

    Update TransactionHistory.jsx

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,29 +1,54 @@
 const TransactionHistory = ({ items }) => {
   return (
-    <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
       {items.map((item) => {
         return (
-          <table key={item.id}>
-            <thead>
-              <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              <tr>
-                <td>{item.type}</td>
-                <td>{item.amount}</td>
-                <td>{item.currency}</td>
-              </tr>
-            </tbody>
-          </table>
+          <tbody key={item.id}>
+            <tr>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          </tbody>
         );
       })}
-    </div>
+    </table>
   );
 };
+
+// const TransactionHistory = ({ items }) => {
+//   return (
+//     <div>
+//       {items.map((item) => {
+//         return (
+//           <table key={item.id}>
+//             <thead>
+//               <tr>
+//                 <th>Type</th>
+//                 <th>Amount</th>
+//                 <th>Currency</th>
+//               </tr>
+//             </thead>
+
+//             <tbody>
+//               <tr>
+//                 <td>{item.type}</td>
+//                 <td>{item.amount}</td>
+//                 <td>{item.currency}</td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
 export default TransactionHistory;
